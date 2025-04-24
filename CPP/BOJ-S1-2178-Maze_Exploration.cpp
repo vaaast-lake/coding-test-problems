12a3b9a13813d63f4fc0a6898bc47c02b94ffd8f
vaaast-lake <kown9512@gmail.com>
2025-04-24 14:39:35 +0900

    solve: BOJ-S1-2178-Maze_Exploration (cpp, js)

--- CPP/BOJ-S1-2178-Maze_Exploration.cpp
+++ CPP/BOJ-S1-2178-Maze_Exploration.cpp
@@ -11,8 +11,8 @@ int main()
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
-  freopen("input.txt", "r", stdin);
-  freopen("output.txt", "w", stdout);
+  // freopen("input.txt", "r", stdin);
+  // freopen("output.txt", "w", stdout);
 
   scanf("%d %d", &n, &m);
   for (int i = 0; i < n; ++i)
--- JS/BOJ-S1-2178-Maze_Exploration.js
+++ JS/BOJ-S1-2178-Maze_Exploration.js
@@ -1,17 +1,43 @@
-"use strict";
+'use strict';
 
-const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "../input.txt";
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : '../input.txt';
 let input = fs
   .readFileSync(filePath)
   .toString()
   .trim()
-  .split("\n")
+  .split('\n')
   .map((v) => v.trimEnd());
 
 let output = [];
-const solution = (input) => {};
+const solution = (input) => {
+  const [n, m] = input[0].split(' ').map(Number);
+  const maze = input.slice(1).map((v) => v.split('').map(Number));
+  const dx = [0, 0, -1, 1];
+  const dy = [-1, 1, 0, 0];
+  const queue = [[0, 0]];
+  maze[0][0] = 2;
+
+  let idx = 0;
+  while (idx < queue.length) {
+    const [x, y] = queue[idx++];
+    for (let i = 0; i < 4; ++i) {
+      const nx = x + dx[i];
+      const ny = y + dy[i];
+      if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+      if (maze[nx][ny] === 1) {
+        queue.push([nx, ny]);
+        maze[nx][ny] = maze[x][y] + 1;
+      }
+    }
+  }
+  output.push(maze[n - 1][m - 1] - 1);
+};
 
 solution(input);
-// fs.writeFileSync("/dev/stdout", output.join("").trimEnd());
-fs.writeFileSync("../output.txt", output.join("").trimEnd(), (err) => console.error);
+fs.writeFileSync('/dev/stdout', output.join('').trimEnd());
+// fs.writeFileSync(
+//   '../output.txt',
+//   output.join('').trimEnd(),
+//   (err) => console.error
+// );
