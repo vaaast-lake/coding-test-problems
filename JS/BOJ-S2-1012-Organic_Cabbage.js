0e4133da69b2b75d105fe4badc3e7b579844d0e1
vaaast-lake <kown9512@gmail.com>
2025-05-01 07:49:02 +0900

    wip(js): BOJ-S2-1012-Organic_Cabbage

---
+++ JS/BOJ-S2-1012-Organic_Cabbage.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : '../input.txt';
+let input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split('\n')
+  .map((v) => v.trimEnd());
+
+let output = [];
+const solution = (input) => {};
+
+solution(input);
+// fs.writeFileSync("/dev/stdout", output.join("").trimEnd());
+fs.writeFileSync(
+  '../output.txt',
+  output.join('').trimEnd(),
+  (err) => console.error
+);
