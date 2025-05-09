6dd495e81fc731678f799e8a79ed1d94c4e7e11f
vaaast-lake <kown9512@gmail.com>
2025-05-09 21:51:45 +0900

    solve(js): BOJ-S2-1012-Organic_Cabbage

--- JS/BOJ-S2-1012-Organic_Cabbage.js
+++ JS/BOJ-S2-1012-Organic_Cabbage.js
@@ -9,65 +9,78 @@ let input = fs
   .split('\n')
   .map((v) => v.trimEnd());
 
-let N, M, K;
-const mp = Array.from({ length: N }, () => 0);
-const visited = Array.from({ length: N }, () =>
-  Array.from({ length: M }, () => 0)
+let N, M, K, ny, nx;
+const mp = Array.from({ length: 51 }, () =>
+  Array.from({ length: 51 }, () => 0)
 );
-
-console.log(mp);
+const visited = Array.from({ length: 51 }, () =>
+  Array.from({ length: 51 }, () => 0)
+);
 
 const dx = [-1, 0, 1, 0];
 const dy = [0, 1, 0, -1];
 const dfs = (y, x) => {
+  visited[y][x] = 1;
+
   for (let i = 0; i < 4; ++i) {
     ny = y + dy[i];
     nx = x + dx[i];
+
     if (ny < 0 || nx < 0 || ny >= N || nx >= M) continue;
-    if (mp[ny][nx] == 1 && !visited[ny][nx]) dfs(ny, nx);
+
+    // console.log(`dfs i: ${i}`);
+    // console.log(`y: ${y}, x: ${x} -> ny: ${ny}, nx: ${nx}`);
+    // console.log(`mp[ny][nx]: ${mp[ny][nx]}`);
+    // console.log(`visited[ny][nx]: ${visited[ny][nx]}`);
+
+    if (mp[ny][nx] === 1 && !visited[ny][nx]) {
+      dfs(ny, nx);
+    }
   }
 };
 
 let output = [];
 const solution = (input) => {
   let T = +input[0];
-  let i = 1;
-  let ret = 0;
+  let idx = 1;
 
   while (T--) {
-    [M, N, K] = input[i].split(' ').map(Number);
+    let ret = 0;
+    [M, N, K] = input[idx].split(' ').map(Number);
     const carrot = input
-      .slice(i + 1, i + K + 1)
+      .slice(idx + 1, idx + K + 1)
       .map((el) => el.split(' ').map(Number));
+
+    for (let i = 0; i < carrot.length; ++i) {
+      mp[carrot[i][1]][carrot[i][0]] = 1;
+    }
+
+    // console.table(mp);
 
     for (let i = 0; i < N; ++i) {
       for (let j = 0; j < M; ++j) {
-        // mp[i][j] = 0;
-        // visited[i][j] = 0;
-      }
-    }
-
-    for (let i = 0; i < carrot.length; ++i) {
-      // mp[carrot[i][0]][carrot[i][1]] = 1;
-    }
-
-    for (let i = 0; i < mp.length; ++i) {
-      for (let j = 0; j < mp[i].length; ++j) {
         if (mp[i][j] === 1 && !visited[i][j]) {
+          // console.log(`\nT${T} -- i: ${i}, j: ${j}`);
           dfs(i, j);
           ++ret;
         }
       }
     }
 
-    i += K + 1;
+    for (let i = 0; i < N; ++i) {
+      mp[i].fill(0);
+      visited[i].fill(0);
+    }
+
+    output.push(ret);
+    idx += K + 1;
   }
 };
 
 solution(input);
-// fs.writeFileSync("/dev/stdout", output.join("").trimEnd());
+// fs.writeFileSync('/dev/stdout', output.join('\n').trimEnd());
 fs.writeFileSync(
   '../output.txt',
-  output.join('').trimEnd(),
+  output.join('\n').trimEnd(),
   (err) => console.error
 );
